d48a24dfddb151b21f6ce151fb9881fcc108d222
Simion Alex <simionalexcristian@gmail.com>
2025-03-01 19:30:08 +0200

    Refactor calculator functionality and replace button handling with expression handling

---
+++ scripts/expressionHandling.js
@@ -0,0 +1,86 @@
+export function resetCalculator(previousOperationElement, currentOperationElement) {
+  previousOperationElement.innerHTML = "";
+  currentOperationElement.innerHTML = "0";
+}
+
+export function renderNumber(action, currentOperationElement) {
+  const lastCharacter = currentOperationElement.innerText.slice(-1);
+  
+  console.log('received');
+  if (action === ',') {
+    const parts = currentOperationElement.innerText.split(/[*+\-/]/);
+    const lastPart = parts[parts.length - 1];
+    if (!lastPart.includes(",") && !["*", "+", "-", "/"].includes(lastCharacter)) {
+      currentOperationElement.innerHTML += action;
+    } else {
+      alert("Invalid operation.");
+    }
+
+  } else if (currentOperationElement.innerText === '0') {
+    currentOperationElement.innerText = action;
+    console.log('updated2');
+    currentOperationElement.innerText = formatExpression(currentOperationElement.innerText);
+  
+  } else {
+    currentOperationElement.innerText += action;
+    console.log('updated3');
+    currentOperationElement.innerText = formatExpression(currentOperationElement.innerText);
+  }
+}
+
+export function renderOperator(action, currentOperationElement) {
+  const lastCharacter = currentOperationElement.innerText.slice(-1);
+
+  if ([',', '-', '+', '/', '*'].includes(lastCharacter)) {
+    currentOperationElement.innerText = currentOperationElement.innerText.slice(0, -1) + action;
+  } else {
+    currentOperationElement.innerText += action;
+  }
+  currentOperationElement.innerText = formatExpression(currentOperationElement.innerText);
+}
+
+/* Functions For Formatting */
+
+/*
+  Formats a mathematical expression by adding dots as thousands separators to the numbers and retaining the operators.
+
+  **Parameters:**
+  - `expression` (String): The mathematical expression to format.
+
+  **Returns:**
+  - (String): The formatted expression.
+*/
+function formatExpression(expression) {
+  return expression
+    .split(/([*+\-/])/)
+    .map((part) => {
+      if (["*", "+", "-", "/"].includes(part)) {
+        return part;
+      } else {
+        return checkNumber(part);
+      }
+    })
+    .join("");
+}
+
+/*
+  Formats a number string by adding dots as thousands separators to the integer part and retaining the comma as the decimal separator.
+
+  **Parameters:**
+  - `string` (String): The number string to format.
+
+  **Returns:**
+  - (String): The formatted number string.
+*/
+function checkNumber(string) {
+  let [integerPart, decimalPart] = string.split(",");
+
+  // Remove existing dots to avoid incorrect formatting
+  integerPart = integerPart.replace(/\./g, "");
+
+  // Add dots as thousands separators
+  integerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+  // Update the currentOperation with the formatted number
+  return decimalPart ? `${integerPart},${decimalPart}` : integerPart;
+}
--- scripts/general.js
+++ scripts/general.js
@@ -1,13 +1,20 @@
-import * as buttonHandling from "./buttonHandling.js";
+import * as expressionHandling from "./expressionHandling.js";
 const previousOperationElement = document.querySelector('.js-previous-text');
 const currentOperationElement = document.querySelector('.js-current-text');
 
+/* Buttons Events Listeners */
+
 document.querySelector('.js-reset-button')
-  .addEventListener('click', () => buttonHandling.resetCalculator(previousOperationElement, currentOperationElement));
+  .addEventListener('click', () => expressionHandling.resetCalculator(previousOperationElement, currentOperationElement));
 
 document.querySelectorAll('.js-number-button')
   .forEach(button => { 
-    const action = button.dataset.action;
-    button.addEventListener('click', () => { buttonHandling.renderButtonAction(action, currentOperationElement) });
-    console.log('event listener added');
+    const action = button.dataset.action; // The number imputed by the user
+    button.addEventListener('click', () => { expressionHandling.renderNumber(action, currentOperationElement) });
+  });
+
+document.querySelectorAll('.js-operation-button')
+  .forEach(button => { 
+    const action = button.dataset.action; // The operator imputed by the user
+    button.addEventListener('click', () => { expressionHandling.renderOperator(action, currentOperationElement) });
   });
